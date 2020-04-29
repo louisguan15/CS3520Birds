524efa3b54f5faacfc6400eab26cf1a8d91f7d8b
geposner <48966870+geposner@users.noreply.github.com>
2020-04-29 11:33:14 -0700

    Rule improvements
    Split search/print rules into search rules and print rules, as well as defining attributes that aren't necessarily atomic as lists. Current implementation shouldn't combinatorially explode with multiple values/attribute now. Four birds currently in database; goal is at least 50 by presentation day. Also, we have an oddly high focus on duck-like birds in the database. We should add more of different kinds, like hawks/pigeons/hummingbirds/owls/perching, etc.

--- birds.pl
+++ birds.pl
@@ -1,92 +1,160 @@
-appearance(Bird, Size, Shape, Color) :-
+contains(Target, [Target|ListRem]).
+contains(Target, [NotTarget|ListRem]) :-
+	contains(Target, ListRem).
+printAppearance(Bird) :-
 	size(Bird, Size),
 	shape(Bird, Shape),
-	color(Bird, Color).
-enviro(Bird, Habitat, Diet, ConStat) :-
-	habitat(Bird, Habitat),
-	diet(Bird, Diet),
+	colorList(Bird, Colors),
+	write('Size: '),
+	writeln(Size),
+	write('Shape: '),
+	writeln(Shape),
+	write('Colors: '),
+	writeln(Colors).
+printEnviro(Bird) :-
+	habitatList(Bird, HabitatL),
+	dietList(Bird, DietL),
+	conStat(Bird, ConStat),
+	write('Habitats Common: '),
+	writeln(HabitatL),
+	write('Primary Diets: '),
+	writeln(DietL),
+	write('Conservation Status: '),
+	writeln(ConStat).
+printBehavior(Bird) :-
+	flightPatList(Bird, FlightPatL),
+	callTypeList(Bird, CallL),
+	write('Flight Pattern Characteristics: '),
+	writeln(FlightPatL),
+	write('Most Common Calls: '),
+	writeln(CallL).
+printCasual(Bird) :-
+	printAppearance(Bird),
+	printBehavior(Bird).
+printBiology(Bird) :-
+	printEnviro(Bird),
+	printBehavior(Bird).
+printEncyclo(Bird) :-
+	printAppearance(Bird),
+	printEnviro(Bird).
+printComprehensive(Bird) :-
+	printEncyclo(Bird),
+	printBehavior(Bird).
+colorSrx(Bird, Color) :-
+	colorList(Bird, ColorL),
+	contains(Color, ColorL).
+habitatSrx(Bird, Habitat) :-
+	habitatList(Bird, HabitatL),
+	contains(Habitat, HabitatL).
+dietSrx(Bird, Diet) :-
+	dietList(Bird, DietL),
+	contains(Diet, DietL).
+flightPatSrx(Bird, FlightPat) :-
+	flightPatList(Bird, FlightPatL),
+	contains(FlightPat, FlightPatL).
+callTypeSrx(Bird, CallType) :-
+	callTypeList(Bird, CallL),
+	contains(Call, CallL).
+appearanceSrx(Bird, Size, Shape, Color) :-
+	size(Bird, Size),
+	shape(Bird, Shape),
+	colorSrx(Bird, Color).
+enviroSrx(Bird, Habitat, Diet, ConStat) :-
+	habitatSrx(Bird, Habitat),
+	dietSrx(Bird, Diet),
 	conStat(Bird, ConStat).
-behavior(Bird, FlightPat, Call) :-
-	flightPat(Bird, FlightPat),
-	callType(Bird, Call).
-casual(Bird, Size, Shape, Color, FlightPat, Call) :-
+behaviorSrx(Bird, FlightPat, Call) :-
+	flightPatSrx(Bird, FlightPat),
+	callTypeSrx(Bird, Call).
+casualSrx(Bird, Size, Shape, Color, FlightPat, Call) :-
 	appearance(Bird, Size, Shape, Color),
 	behavior(Bird, FlightPat, Call).
-biology(Bird, Habitat, Diet, ConStat, FlightPat, Call) :-
+biologySrx(Bird, Habitat, Diet, ConStat, FlightPat, Call) :-
 	enviro(Bird, Habitat, Diet, ConStat),
 	behavior(Bird, FlightPat, Call).
-encyclo(Bird, Size, Shape, Color, Habitat, Diet, ConStat) :-
+encycloSrx(Bird, Size, Shape, Color, Habitat, Diet, ConStat) :-
 	appearance(Bird, Size, Shape, Color),
 	enviro(Bird, Habitat, Diet, ConStat).
-comprehensive(Bird, Size, Shape, Color, Habitat, Diet, ConStat, FlightPat, Call) :-
+comprehensiveSrx(Bird, Size, Shape, Color, Habitat, Diet, ConStat, FlightPat, Call) :-
 	encyclo(Bird, Size, Shape, Color, Habitat, Diet, ConStat),
 	behavior(Bird, FlightPat, Call).
-simSize(InBird, OutBird) :-
+sameSize(InBird, OutBird) :-
 	size(InBird, Size),
 	size(OutBird, Size).
-simShape(InBird, OutBird) :-
+sameShape(InBird, OutBird) :-
 	shape(InBird, Shape),
 	shape(OutBird, Shape).
-simColor(InBird, OutBird) :-
-	color(InBird, Color),
-	color(OutBird, Color).
-simHabitat(InBird, OutBird) :-
-	habitat(InBird, Habitat),
-	habitat(OutBird, Habitat).
-simDiet(InBird, OutBird) :-
-	diet(InBird, Diet),
-	diet(OutBird, Diet).
-simConStat(InBird, OutBird) :-
+sameColor(InBird, OutBird) :-
+	colorList(InBird, ColorL),
+	colorList(OutBird, ColorL).
+sameHabitat(InBird, OutBird) :-
+	habitatList(InBird, HabitatL),
+	habitatList(OutBird, HabitatL).
+sameDiet(InBird, OutBird) :-
+	dietList(InBird, DietL),
+	dietList(OutBird, DietL).
+sameConStat(InBird, OutBird) :-
 	conStat(InBird, ConStat),
 	conStat(OutBird, ConStat).
-simFlight(InBird, OutBird) :-
-	flightPat(InBird, FlightPat),
-	flightPat(OutBird, FlightPat).
-simCall(InBird, OutBird) :-
-	callType(InBird, Call),
-	callType(OutBird, Call).
-simAppearance(InBird, OutBird) :-
-	simSize(InBird, OutBird),
-	simShape(InBird, OutBird),
-	simColor(InBird, OutBird).
-simEnviro(InBird, OutBird) :-
-	simHabitat(InBird, OutBird),
-	simDiet(InBird, OutBird),
-	simConStat(InBird, OutBird).
-simBehavior(InBird, OutBird) :-
-	simFlight(InBird, OutBird),
-	simCall(InBird, OutBird).
-simCasual(InBird, OutBird) :-
-	simAppearance(InBird, OutBird),
-	simBehavior(InBird, OutBird).
-simBiology(InBird, OutBird) :-
-	simEnviro(InBird, OutBird),
-	simBehavior(InBird, OutBird).
-simEncyclo(InBird, OutBird) :-
-	simAppearance(InBird, OutBird),
-	simEnviro(InBird, OutBird).
-simComp(InBird, OutBird) :-
-	simEncyclo(InBird, OutBird),
-	simBehavior(InBird, OutBird).
+sameFlight(InBird, OutBird) :-
+	flightPatList(InBird, FlightPatL),
+	flightPatList(OutBird, FlightPatL).
+sameCall(InBird, OutBird) :-
+	callTypeList(InBird, CallL),
+	callTypeList(OutBird, CallL).
+sameAppearance(InBird, OutBird) :-
+	sameSize(InBird, OutBird),
+	sameShape(InBird, OutBird),
+	sameColor(InBird, OutBird).
+sameEnviro(InBird, OutBird) :-
+	sameHabitat(InBird, OutBird),
+	sameDiet(InBird, OutBird),
+	sameConStat(InBird, OutBird).
+sameBehavior(InBird, OutBird) :-
+	sameFlight(InBird, OutBird),
+	sameCall(InBird, OutBird).
+sameCasual(InBird, OutBird) :-
+	sameAppearance(InBird, OutBird),
+	sameBehavior(InBird, OutBird).
+sameBiology(InBird, OutBird) :-
+	sameEnviro(InBird, OutBird),
+	sameBehavior(InBird, OutBird).
+sameEncyclo(InBird, OutBird) :-
+	sameAppearance(InBird, OutBird),
+	sameEnviro(InBird, OutBird).
+sameComp(InBird, OutBird) :-
+	sameEncyclo(InBird, OutBird),
+	sameBehavior(InBird, OutBird).
 
 size(mallard, large).
 size(lesser_scaup, large).
+size(long-tailed_duck, large).
+size(mountain_quail, medium).
 shape(mallard, duck).
 shape(lesser_scaup, duck).
-color(mallard, grey).
-color(lesser_scaup, grey).
-color(lesser_scaup, black).
-habitat(mallard, wetland).
-habitat(lesser_scaup, wetland).
-diet(mallard, omnivorous).
-diet(lesser_scaup, insectivorous).
-diet(lesser_scaup, herbivorous).
+shape(long-tailed_duck, duck).
+shape(mountain_quail, neoquail).
+colorList(mallard, [grey]).
+colorList(lesser_scaup, [grey,black]).
+colorList(long-tailed_duck, [white,black]).
+colorList(mountain_quail, [brown,grey]).
+habitatList(mallard, [wetlands]).
+habitatList(lesser_scaup, [wetlands]).
+habitatList(long-tailed_duck, [wetlands]).
+habitatList(mountain_quail, [chaparral]).
+dietList(mallard, [omnivorous]).
+dietList(lesser_scaup, [insectivorous,herbivorous]).
+dietList(long-tailed_duck, [insectivorous,piscivorous]).
+dietList(mountain_quail, [herbivorous]).
 conStat(mallard, lc).
 conStat(lesser_scaup, lc).
-flightPat(mallard, strongFlap).
-flightPat(mallard, direct).
-flightPat(lesser_scaup, rapidFlap).
-flightPat(lesser_scaup, direct).
-callType(mallard, quacking).
-callType(lesser_scaup, whistling).
-callType(lesser_scaup, trilling).
+conStat(long-tailed_duck, vu).
+conStat(mountain_quail, lc).
+flightPatList(mallard, [strongFlap,direct]).
+flightPatList(lesser_scaup, [rapidFlap,direct]).
+flightPatList(long-tailed_duck, [direct,rapidTurn]).
+flightPatList(mountain_quail, [strongFlap,gliding]).
+callTypeList(mallard, [quacking]).
+callTypeList(lesser_scaup, [whistling,trilling]).
+callTypeList(long-tailed_duck, [honking]).
+callTypeList(mountain_quail, [whistling,chirping]).
